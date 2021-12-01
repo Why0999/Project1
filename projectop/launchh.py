38825b358d56e8160dc4dfc30d4669ef3c39a5ff
Why0999 <39419580+Why0999@users.noreply.github.com>
2021-12-01 11:18:44 -0500

    Update launchh.py

--- projectop/launchh.py
+++ projectop/launchh.py
@@ -1,7 +1,32 @@
 from os import truncate
 from flask import Flask, render_template, url_for, flash, redirect
 
+from flask import jsonify
+from flask import request
+from flask import make_response
+import pymysql.cursors 
+from flask_cors import CORS,cross_origin
+import string
+import random
+from datetime import *
+
 app = Flask(__name__)
+
+config = {
+  'ORIGINS': [
+    'http://localhost:3000',  # React
+    'http://127.0.0.1:3000',  # React
+  ],
+}
+
+CORS(app,resources={ r'/*': {'origins': config['ORIGINS']}}, supports_credentials=True)
+ 
+conn = pymysql.connect(host='localhost',
+                       user='ronal',
+                       password='password',
+                       db='Vac',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
 
 @app.route("/")
 def home():
